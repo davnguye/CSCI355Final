43a3b244b9317d9e56b3f2feab368f6285802469
mxcomer <45543586+mxcomer@users.noreply.github.com>
2019-12-02 12:39:44 -0500

    Fixed indentation

--- SLGraph/SLGraph.js
+++ SLGraph/SLGraph.js
@@ -77,125 +77,126 @@ class SLNode{
 
 
 class SLGraph{
-    /*
-        ====================
-            CONSTRUCTOR
-        ====================
-    */
-    
-        constructor(){
-    
-            this.nodes = [];
-            this.edges = [];
-    
-        }//end constructor
-    
-    /*
-        ====================
-            PROPERTIES
-        ====================
-    
-    */
-        nodes = [];
-        edges = [];
-    
-    /*
-        ====================
-              METHODS
-        ====================
-    */
+/*
+    ====================
+        CONSTRUCTOR
+    ====================
+*/
 
-        //Method to load node data from JSON file
-        //Should only be used once with initial data
-        populateNodes(){
-            this.nodes = [];
-            //Using node.js file sync to read in node data
-            var fs = require('fs');
-            //Reading JSON file into string
-            var data = fs.readFileSync('./nodes.json', 'utf8');
-            //Parsing JSON string into object array
-            var nodeArray = JSON.parse(data);
-            //Populating node array
-            nodeArray.forEach(node => {
-                let newNode = new SLNode(node.x, node.y, node.id, node.label);
-                this.nodes.push(newNode);
-            });
-            //Creating node relationships
-            this.addNodeRel();
-        }//end populate nodes
-            
-        //Method for creating node relationships
-        addNodeRel(){
-            //Origin (LD Stairwell)
-            this.nodes[0].setNeighbor(this.nodes[3]);
-            //West elevator
-            this.nodes[1].setNeighbor(this.nodes[3]);
-            //East elevator
-            this.nodes[2].setNeighbor(this.nodes[3]);
-            //Stairway intersection (I0)
-            this.nodes[3].setNeighbor(this.nodes[0], this.nodes[1], this.nodes[2], this.nodes[4], this.nodes[17]);
-            //LD intersection (I1)
-            this.nodes[4].setNeighbor(this.nodes[3], this.nodes[14]);
-            //South hall intersection
-            this.nodes[5].setNeighbor(this.nodes[6], this.nodes[7]);
-            //Southwest corner
-            this.nodes[6].setNeighbor(this.nodes[5], this.nodes[10]);
-            //Southeast corner
-            this.nodes[7].setNeighbor(this.nodes[5], this.nodes[13]);
-            //Northwest corner
-            this.nodes[8].setNeighbor(this.nodes[10], this.nodes[11], this.nodes[15]);
-            //Northeast corner
-            this.nodes[9].setNeighbor(this.nodes[12], this.nodes[13], this.nodes[16]);
-            //Bathroom intersection
-            this.nodes[10].setNeighbor(this.nodes[6], this.nodes[8], this.nodes[18], this.nodes[19]);
-            //SL247 intersection
-            this.nodes[11].setNeighbor(this.nodes[8], this.nodes[12], this.nodes[20]);
-            //SL251 intersection
-            this.nodes[12].setNeighbor(this.nodes[9], this.nodes[11], this.nodes[21]);
-            //SL280 intersection
-            this.nodes[13].setNeighbor(this.nodes[7], this.nodes[9], this.nodes[22]);
-            //LD exit
-            this.nodes[14].setNeighbor(this.nodes[4], this.nodes[5]);
-            //West exit
-            this.nodes[15].setNeighbor(this.nodes[8]);
-            //East exit
-            this.nodes[16].setNeighbor(this.nodes[9]);
-            //Student lounge
-            this.nodes[17].setNeighbor(this.nodes[3]);
-            //Women's restroom
-            this.nodes[18].setNeighbor(this.nodes[10]);
-            //Men's restroom
-            this.nodes[19].setNeighbor(this.nodes[10]);
-            //SL247
-            this.nodes[20].setNeighbor(this.nodes[11]);
-            //SL251
-            this.nodes[21].setNeighbor(this.nodes[12]);
-            //SL280
-            this.nodes[22].setNeighbor(this.nodes[13]);
-        }//end addNodeNeighbor
+    constructor(){
 
-        //Method to add graph edge
-        populateEdges(){
-            this.edges = [];
-            var id = 0;
-            //Creating a path from each node to its neighboring nodes
-            //Adds two-way paths
-            this.nodes.forEach(node => {
-                node.neighbors.forEach( neighbor=> {
-                    this.edges.push({id: "G"+id, source:node.id, target: neighbor.neighbor.id, weight: neighbor.distance});
-                    id++;
-                });
-            });
-        }//end populate edges
+        this.nodes = [];
+        this.edges = [];
 
-        //Method to create JSON file with graph data
-       /* createGraphJSON(){
-            var graphJSON = JSON.stringify(this.edges);
-            var fs = require('fs');
-            fs.writeFile("graph.json", graphJSON, function(error, result){
-                if(error) console.log('error', error);
+    }//end constructor
+
+/*
+    ====================
+        PROPERTIES
+    ====================
+
+*/
+    nodes = [];
+    edges = [];
+
+/*
+    ====================
+            METHODS
+    ====================
+*/
+
+    //Method to load node data from JSON file
+    //Should only be used once with initial data
+    populateNodes(){
+        this.nodes = [];
+        //Using node.js file sync to read in node data
+        var fs = require('fs');
+        //Reading JSON file into string
+        var data = fs.readFileSync('./nodes.json', 'utf8');
+        //Parsing JSON string into object array
+        var nodeArray = JSON.parse(data);
+        //Populating node array
+        nodeArray.forEach(node => {
+            let newNode = new SLNode(node.x, node.y, node.id, node.label);
+            this.nodes.push(newNode);
+        });
+        //Creating node relationships
+        this.addNodeRel();
+    }//end populate nodes
+        
+    //Method for creating node relationships
+    addNodeRel(){
+        //Origin (LD Stairwell)
+        this.nodes[0].setNeighbor(this.nodes[3]);
+        //West elevator
+        this.nodes[1].setNeighbor(this.nodes[3]);
+        //East elevator
+        this.nodes[2].setNeighbor(this.nodes[3]);
+        //Stairway intersection (I0)
+        this.nodes[3].setNeighbor(this.nodes[0], this.nodes[1], this.nodes[2], this.nodes[4], this.nodes[17]);
+        //LD intersection (I1)
+        this.nodes[4].setNeighbor(this.nodes[3], this.nodes[14]);
+        //South hall intersection
+        this.nodes[5].setNeighbor(this.nodes[6], this.nodes[7]);
+        //Southwest corner
+        this.nodes[6].setNeighbor(this.nodes[5], this.nodes[10]);
+        //Southeast corner
+        this.nodes[7].setNeighbor(this.nodes[5], this.nodes[13]);
+        //Northwest corner
+        this.nodes[8].setNeighbor(this.nodes[10], this.nodes[11], this.nodes[15]);
+        //Northeast corner
+        this.nodes[9].setNeighbor(this.nodes[12], this.nodes[13], this.nodes[16]);
+        //Bathroom intersection
+        this.nodes[10].setNeighbor(this.nodes[6], this.nodes[8], this.nodes[18], this.nodes[19]);
+        //SL247 intersection
+        this.nodes[11].setNeighbor(this.nodes[8], this.nodes[12], this.nodes[20]);
+        //SL251 intersection
+        this.nodes[12].setNeighbor(this.nodes[9], this.nodes[11], this.nodes[21]);
+        //SL280 intersection
+        this.nodes[13].setNeighbor(this.nodes[7], this.nodes[9], this.nodes[22]);
+        //LD exit
+        this.nodes[14].setNeighbor(this.nodes[4], this.nodes[5]);
+        //West exit
+        this.nodes[15].setNeighbor(this.nodes[8]);
+        //East exit
+        this.nodes[16].setNeighbor(this.nodes[9]);
+        //Student lounge
+        this.nodes[17].setNeighbor(this.nodes[3]);
+        //Women's restroom
+        this.nodes[18].setNeighbor(this.nodes[10]);
+        //Men's restroom
+        this.nodes[19].setNeighbor(this.nodes[10]);
+        //SL247
+        this.nodes[20].setNeighbor(this.nodes[11]);
+        //SL251
+        this.nodes[21].setNeighbor(this.nodes[12]);
+        //SL280
+        this.nodes[22].setNeighbor(this.nodes[13]);
+    }//end addNodeNeighbor
+
+    //Method to add graph edge
+    populateEdges(){
+        this.edges = [];
+        var id = 0;
+        //Creating a path from each node to its neighboring nodes
+        //Adds two-way paths
+        this.nodes.forEach(node => {
+            node.neighbors.forEach( neighbor=> {
+                this.edges.push({id: "G"+id, source:node.id, target: neighbor.neighbor.id, weight: neighbor.distance});
+                id++;
             });
-        }//end create graph JSON
-    */
+        });
+    }//end populate edges
+
+    //Method to create JSON file with graph data
+    /* createGraphJSON(){
+        var graphJSON = JSON.stringify(this.edges);
+        var fs = require('fs');
+        fs.writeFile("graph.json", graphJSON, function(error, result){
+            if(error) console.log('error', error);
+        });
+    }//end create graph JSON
+*/
+    
   
 }//end SL Graph class   
